9f00e44360e6c6e825b76aefe96705848bf7bf82
Shrujal Tailor <64884667+shrujal23@users.noreply.github.com>
2021-05-02 15:13:02 +0530

    Delete Programs directory

--- Programs/PR1.java
+++
@@ -1,6 +0,0 @@
-/*PR-1:- Write a Program that displays Welcome to Java, Learning Java Now and Programming is fun.*/
-class PR1{  
-    public static void main(String args[]){  
-     System.out.println("Welcome to Java, Learning Java Now and Programming is fun");  
-    }  
-}
